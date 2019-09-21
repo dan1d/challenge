d8339e69f6d4c4737a108e36cf61d17aaab8e934
Daniel <danielfromarg@gmail.com>
2019-09-21 00:40:45 -0300

    Add admin styles.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 [Movie, Person, MoviePersonRole].map(&:delete_all)
 
+admin = Admin.create(email: "test@admin.com", password: "testadmin")
+
 movies = FactoryBot.create_list(:movie, 10)
 persons = FactoryBot.create_list(:person, 10)
 
